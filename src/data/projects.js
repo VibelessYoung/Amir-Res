fe044c7baea604ea68abdb5009410a89b1e17df3
Amir <vibelessyoung@gmail.com>
2026-07-24 08:13:47 +0330

    Refactor projects section

---
+++ src/data/projects.js
@@ -0,0 +1,33 @@
+export const projects = [
+  {
+    title: "Twitter",
+    description: "شبیه سازی سایت تویتر با API همراه قابلیت ارسال نظر و فیلتر.",
+    image: "/images/twitter.jpg",
+    alt: "Twitter",
+    url: "https://twitter-web-clone-dusky.vercel.app/",
+  },
+
+  {
+    title: "OCR Extractor",
+    description: "استخراج متن از تصاویر با رابط کاربری مدرن.",
+    image: "/images/text.jpg",
+    alt: "OCR",
+    url: "https://ocr-extractor-web-page.vercel.app/",
+  },
+
+  {
+    title: "Saturn Anime",
+    description: "نمایش اطلاعات انیمه با استفاده از API.",
+    image: "/images/scarlet.jpg",
+    alt: "Saturn",
+    url: "https://saturn-anime.vercel.app/",
+  },
+
+  {
+    title: "JobRemote",
+    description: "وبسایت جستجوی شغل همراه قابلیت سرچ.",
+    image: "/images/search.jpg",
+    alt: "Job",
+    url: "https://jobs-remot-web-page.vercel.app/",
+  },
+];
